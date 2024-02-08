d29ef1f27005092a891d210ad6232d8983b53fd6
calderar0 <felipecalderaro28@gmail.com>
2024-02-08 16:32:34 -0300

    Correção de bug, tentativa de forma diferente

--- js/script.js
+++ js/script.js
@@ -4,11 +4,9 @@ function toggleMode() {
 
     if(html.classList.contains('light')){
         html.classList.remove('light')
-        img.setAttribute('src', '/css/images/Avatar.png')
         img.setAttribute('alt', 'avatar1, choice of user')
     }else {
         html.classList.add('light')
-        img.setAttribute('src', '/css/images/avatar2.png')   
         img.setAttribute('alt', 'avatar2, choice of user')    
     }
 }
