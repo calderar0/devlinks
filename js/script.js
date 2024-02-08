9cc9b64b89d5e2098f4b0c160000de87c7bb2797
calderar0 <felipecalderaro28@gmail.com>
2024-02-08 16:00:33 -0300

    Projeto Finalizado

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+    const html = document.documentElement;
+    const img = document.querySelector('#profile img')
+
+    if(html.classList.contains('light')){
+        html.classList.remove('light')
+        img.setAttribute('src', '/css/images/Avatar.png')
+        img.setAttribute('alt', 'avatar1, choice of user')
+    }else {
+        html.classList.add('light')
+        img.setAttribute('src', '/css/images/avatar2.png')   
+        img.setAttribute('alt', 'avatar2, choice of user')    
+    }
+}
