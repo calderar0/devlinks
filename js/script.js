bbad45ec74a510dee227cc441dea667c23d59c5b
calderar0 <felipecalderaro28@gmail.com>
2024-02-08 17:04:48 -0300

    alteração de caminhos, projeto finalizado

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ function toggleMode() {
     const img = document.querySelector('#profile img')
     if(html.classList.contains('light')){
         html.classList.remove('light')
-        img.setAttribute('src', 'images/Avatar.png')
+        img.setAttribute('src', './images/Avatar.png')
         img.setAttribute('alt', 'avatar1, choice of user')
     }else {
         html.classList.add('light')
-        img.setAttribute('src', 'images/avatar2.png')   
+        img.setAttribute('src', './images/avatar2.png')   
         img.setAttribute('alt', 'avatar2, choice of user')    
     }
 }
